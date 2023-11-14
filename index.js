4e9cb0b608bbee64b2af6800f48b37de60c45fdb
movarnell <movarnell@gmail.com>
2023-11-14 10:37:31 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+// Problem 1
+if (x > 10)
+    console.log("x is greater than 10");
+
+// Problem 2
+if (x = 5) {
+    console.log("x is five");
+}
+
+// Problem 3
+let x = 10
+console.log(x)
+
+// Problem 4
+function test() {
+    var x = 5;
+}
+console.log(x);
+
+// Problem 5
+for (let i = 0; i < 10; i--) {
+    console.log(i);
+}
+
+// Problem 6
+console.log(y);
+let y = 10;
+
+// Problem 7
+let x = "5";
+if (x === 5) {
+    console.log("x is five");
+}
+
+// Problem 8
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5));
+
+// Problem 9
+function multiply(a, b) {
+    let result = a * b;
+}
+console.log(multiply(5, 3));
+
+// Problem 10
+let text = 'This is a test string;
+console.log(text);
+
+// Problem 11
+let x = "10";
+if (x == 10) {
+    console.log("x is ten");
+}
+
+// Problem 12
+let obj = {name: "John"};
+console.log(obj[Name]);
+
+// Problem 13
+let array = [1, 2, 3, 4, 5];
+for (let i = 0; i <= array.length; i++) {
+    console.log(array[i]);
+}
+
+// Problem 14
+let x = 10;
+let x = 20;
+
+// Problem 15
+function printMessage() {
+    console.log(message);
+}
+printMessage("Hello, world!");
